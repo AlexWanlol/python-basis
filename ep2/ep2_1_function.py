7f5a3ea2912d2bdf3affc89f9c161d8a459ee4fd
AlexWanlol <euloyui@163.com>
2018-10-25 16:53:56 +0800

    第二章节，函数相关

--- ep2/ep2_1_function.py
+++ ep2/ep2_1_function.py
@@ -1,4 +1,6 @@
 #自定函数 使用def关键字
+#此章节配合ep2.ep2_1_function食用
+
 def eating(type):
     if type=='meat':
         return '肉'
@@ -15,3 +17,34 @@ def emptyFunc():
 # def catchError(x):
 #     if not isinstance(x,(int,float)):
 #         raise TypeError
+
+#测试用的函数
+def power(x,n=2):
+    value = 1
+    while n > 0:
+        n = n - 1
+        value = value * x
+    return value
+
+#可变参数
+def strString(*list):
+    words = 0
+    for word in list:
+        words = words + word * word
+    print(words)
+
+#关键字参数
+def keywords(name,sex,**params):
+    print(name,sex,params)
+
+# *param为可变参数，可包含多个key=value样式的参数， age与city参数规定了*param参数列表中必须存在age,city两个参数关键字(key)
+#可对age,city设置默认值跳过关键字校验
+def changewords(name,sex,*params,age,city):
+    print(name,sex,age,city)
+
+#组合函数
+def mix1(name,sex,*changewords,age,city,**keywords):
+    print(name,sex,changewords,age,city,keywords)
+def mix2(name,sex,*changewords,**keywords):
+    print(name,sex,changewords,keywords)
+
--- ep2/ep2_2_importfunc.py
+++ ep2/ep2_2_importfunc.py
@@ -1,5 +1,22 @@
 #外部调用已经定义好的function
 from ep2.ep2_1_function import eating
+from ep2.ep2_1_function import power
+from ep2.ep2_1_function import strString
+from ep2.ep2_1_function import changewords
+from ep2.ep2_1_function import keywords
+from ep2.ep2_1_function import mix1
+from ep2.ep2_1_function import mix2
 
 #调用function
 print(eating('meat'))
+
+print(power(5))
+
+print(power(5,6))
+
+strString(21312,2134.2)
+
+keywords('FreyWan','male',age=25,city='Wuxi')
+changewords('FreyWan','male',age=25,city='Wuxi')
+mix1('FreyWan','male','whatever',age=25,city='Wuxi')
+mix2('FreyWan','male','whatever',age=25,city='Wuxi')
